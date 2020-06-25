36c6a4b02af94087948a928d849002dad1bc614e
AndreasGassmann <andreas@andreasgassmann.ch>
2020-06-25 20:26:06 +0200

    Updates

---
+++ precache-manifest.56dff8ce9a8e006c890247f2f34db1ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efa2bd35ac679ef98d382ec60482a4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ad18da051747ec3b40c",
+    "url": "/static/js/2.be70f481.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.be70f481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567c5b07e0a232284b7c",
+    "url": "/static/js/main.cf59ef78.chunk.js"
+  },
+  {
+    "revision": "dfe3d817a9ac0c05d2c9",
+    "url": "/static/js/runtime-main.84cfcdfc.js"
+  },
+  {
+    "revision": "38eff924dc699f1ae5aacaf376f5e9e4",
+    "url": "/static/media/airgap_logo.38eff924.svg"
+  },
+  {
+    "revision": "944018ec47b40ed55234afbcd5c67398",
+    "url": "/static/media/beacon-logo.944018ec.svg"
+  },
+  {
+    "revision": "476721bad9ed612b3d8d46cb4ce79a91",
+    "url": "/static/media/logo.476721ba.png"
+  },
+  {
+    "revision": "48a50fb31ef35ab6e028f9fdd3acc534",
+    "url": "/static/media/name.48a50fb3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tztip.me/precache-manifest.42472cc3096deb27bc0b2b6ab68f0b6f.js"
+  "/precache-manifest.56dff8ce9a8e006c890247f2f34db1ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tztip.me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
